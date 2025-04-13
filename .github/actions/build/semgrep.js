f2a504c36ff7b9b8b5f47d99c3e24c52e814fc44
GeoDerp <18461782+GeoDerp@users.noreply.github.com>
2025-04-13 00:01:58 +0000

    test(semgrep): test semgrep action

--- .github/actions/build/semgrep.js
+++ .github/actions/build/semgrep.js
@@ -4,7 +4,6 @@ var data = JSON.parse(fs.readFileSync("./semgrep", "utf8"));
 html = "<h1>Semgrep Results :test_tube:</h1>";
 
 for (var i = 0; i < data.results.length; i++) {
-  if (data.results[i].extra.severity == "ERROR" || data.results[i].extra.severity == "HIGH" || data.results[i].extra.severity == "CRITICAL") {
     html += "<table>";
     html += " <tr> <th>likelihood</th> <th>Implact</th> <th>CWE</th> <th>path</th> <th>start:end line</th> <th>technology</th> </tr>";
     html += "<td>" + data.results[i].extra.metadata.likelihood + "</td>";
@@ -17,5 +16,4 @@ for (var i = 0; i < data.results.length; i++) {
     html += "<p>" + data.results[i].extra.message + "</p>";
     html += "<br>";
   }
-}
 console.log(html);
